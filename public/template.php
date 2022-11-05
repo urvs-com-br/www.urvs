bc4226db2bd457fc472d1f8d16591b856b11cde8
Éverton M. Vieira <emuvi@outlook.com.br>
2022-11-05 20:00:22 -0300

    adds template definition

---
+++ public/template.php
@@ -0,0 +1,16 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php include "head.php" ?>
+</head>
+
+<body>
+  <div id="divMain">
+    <?php include "topbar.php" ?>
+    <?php include "botbar.php" ?>
+  </div>
+</body>
+
+</html>
